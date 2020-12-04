df0e26e9ac1449bd667c4347817a79ffc19a3457
Daniel <55301388+danielfarah54@users.noreply.github.com>
2020-12-03 22:28:03 -0300

    Inclusão de métodos

--- v_0.3/src/main/webapp/js/ajaxListagemPorCampoSelecao.js
+++ v_0.3/src/main/webapp/js/ajaxListagemPorCampoSelecao.js
@@ -5,7 +5,11 @@ function apresenta() {
     var hotel = document.getElementById("hotel");
     var selCidade = cidade.options[cidade.selectedIndex].value; 
 	var selhotel = hotel.options[hotel.selectedIndex].value;
-    alert("Hotel selecionado: " + selhotel); 
+    alert("Hotel selecionado: " + selhotel);
+    var selEmail = document.getElementById("email");
+    selEmail.innerHTML = atualizaEmail(selhotel);
+    var selCnpj = document.getElementById("cnpj");
+    selCnpj.innerHTML = atualizaCnpj(selhotel);
 }
 
 function cidadeSelecionada(str) {
@@ -31,4 +35,50 @@ function atualizaCidades() {
     if (xmlHttp.readyState === 4 || xmlHttp.readyState === "complete") {
         document.getElementById("hotel").innerHTML = xmlHttp.responseText;
     }
+}
+
+function atualizaEmail(str) {
+    if (typeof XMLHttpRequest !== "undefined") {
+        xmlHttp = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    if (xmlHttp === null) {
+        alert("Browser does not support XMLHTTP Request");
+        return;
+    }
+
+    var url = "MostraDadosHotelSelecionadoController";
+    url += "?hotel=" + str + "&emailsel=true&cnpjsel=false";
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState == 4 || xmlHttp.readyState === "complete") {
+            document.getElementById("email").innerHTML = this.responseText;
+        }
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+
+function atualizaCnpj(str) {
+    if (typeof XMLHttpRequest !== "undefined") {
+        xmlHttp = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    if (xmlHttp === null) {
+        alert("Browser does not support XMLHTTP Request");
+        return;
+    }
+
+    var url = "MostraDadosHotelSelecionadoController";
+    url += "?hotel=" + str + "&emailsel=false&cnpjsel=true";
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState == 4 || xmlHttp.readyState === "complete") {
+            document.getElementById("cnpj").innerHTML = this.responseText;
+        }
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
 }
