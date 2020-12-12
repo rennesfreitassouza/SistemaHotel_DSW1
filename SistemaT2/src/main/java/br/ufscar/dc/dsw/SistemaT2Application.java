52079bd5e18d284c7f82313cf0dd779f10427a9c
Rennes Freitas Souza <49644107+rennesfreitassouza@users.noreply.github.com>
2020-12-12 17:20:25 -0300

    Add files via upload
    Início do projeto para o trabalho 2. pom.xml atualizado.

---
+++ SistemaT2/src/main/java/br/ufscar/dc/dsw/SistemaT2Application.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaT2Application.class, args);
+	}
+
+}
