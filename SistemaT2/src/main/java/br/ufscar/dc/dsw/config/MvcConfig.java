85fe0fd14677f4788d59d627374832754a5196a0
Luís Souza <luis.souzaf@hotmail.com>
2021-01-06 23:02:51 -0300

    Sistema Login adicionado

--- SistemaT2/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
+++ SistemaT2/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
@@ -1,9 +1,17 @@
 package br.ufscar.dc.dsw.config;
 
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import br.ufscar.dc.dsw.conversor.BigDecimalConversor;
 
@@ -12,6 +20,34 @@ import br.ufscar.dc.dsw.conversor.BigDecimalConversor;
 @ComponentScan(basePackages = "br.ufscar.dc.dsw.config")
 public class MvcConfig implements WebMvcConfigurer {
     
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/admin").setViewName("admin/index");
+		registry.addViewController("/hotel").setViewName("hotel/index");
+		registry.addViewController("/sitereserva").setViewName("sitereserva/index");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("pt","BR"));
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+	
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new BigDecimalConversor());
